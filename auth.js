8a97f37a9c607d86c42a7e0201bddcdeb17fab3b
ReQueiroz <159490585+ReQueiroz@users.noreply.github.com>
2025-04-29 22:09:34 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -2,33 +2,43 @@
 
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBlY1s_OQKLGFJaZSyDJzKIoX3WnFGSq58",
-    authDomain: "intranetcomfrio.firebaseapp.com",
-    projectId: "intranetcomfrio",
-    storageBucket: "intranetcomfrio.appspot.com",
-    messagingSenderId: "347790318348",
-    appId: "1:347790318348:web:901f10beadae6981e40729"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
-  // Verificar o estado de autenticação
+  apiKey: "AIzaSyBlY1s_OQKLGFJaZSyDJzKIoX3WnFGSq58",
+  authDomain: "intranetcomfrio.firebaseapp.com",
+  projectId: "intranetcomfrio",
+  storageBucket: "intranetcomfrio.appspot.com",
+  messagingSenderId: "347790318348",
+  appId: "1:347790318348:web:901f10beadae6981e40729"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Função de verificação de autenticação
+function checkAuth() {
   firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // Se estiver autenticado, exibe o nome do usuário e a opção de logout
-      document.getElementById("loginDropdown").innerHTML = `Login: ${user.displayName || 'Usuário'} <i class="fas fa-cog"></i>`;
-      document.getElementById("sairBtn").style.display = 'block';
+    if (!user) {
+      // Usuário não autenticado
+      window.location.href = 'index.html';
     } else {
-      // Se não estiver autenticado, exibe apenas a opção de login
-      document.getElementById("loginDropdown").innerHTML = `Login: ADMIN <i class="fas fa-cog"></i>`;
-      document.getElementById("sairBtn").style.display = 'none';
+      // Autenticado, exibe conteúdo
+      document.body.style.display = 'block';
+
+      // Se os elementos existirem, exibe nome de usuário e botão de logout
+      const loginDropdown = document.getElementById("loginDropdown");
+      const sairBtn = document.getElementById("sairBtn");
+
+      if (loginDropdown) {
+        loginDropdown.innerHTML = `Login: ${user.displayName || 'Usuário'} <i class="fas fa-cog"></i>`;
+      }
+      if (sairBtn) {
+        sairBtn.style.display = 'block';
+        sairBtn.addEventListener("click", function () {
+          firebase.auth().signOut().then(function () {
+            window.location.href = 'index.html';
+          }).catch(function(error) {
+            console.error("Erro ao sair: ", error);
+          });
+        });
+      }
     }
   });
-  
-  // Ação de logout
-  document.getElementById("sairBtn").addEventListener("click", function () {
-    firebase.auth().signOut().then(function () {
-      window.location.href = 'index.html';  // Redireciona para a página de login
-    }).catch(function(error) {
-      console.error("Erro ao sair: ", error);
-    });
-  });
+}
